9102ba43a70b9f9eade4a8cd95762a1b9c28fa54
Andrew Fager <graveviolin@gmail.com>
2014-05-31 15:53:51 -0600

    Added .gitignore, and modified settings and wsgi for heroku support.

--- Backend/movemymusic2/settings.py
+++ Backend/movemymusic2/settings.py
@@ -55,16 +55,16 @@ WSGI_APPLICATION = 'movemymusic2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'movemymusicdb',
-	'USER': 'musicadmin',
-	'PASSWORD': 'musicrocks',
-	'HOST': '',
-	'PORT': '',
-    }
-}
+#DATABASES = {
+ #   'default': {
+ #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ #        'NAME': 'movemymusicdb',
+	# 'USER': 'musicadmin',
+	# 'PASSWORD': 'musicrocks',
+	# 'HOST': '',
+	# 'PORT': '',
+ #    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -84,3 +84,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
+
