7f2619241f0f90673c1e3934dccd64b646d764a1
Andrew Fager <graveviolin@gmail.com>
2014-06-01 09:39:21 -0600

    Getting Post/URLs working

--- Backend/movemymusic2/urls.py
+++ Backend/movemymusic2/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.contrib import admin
+from movemymusic2 import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('', 
-	url(r'^teacher/$', 'teacher.views'),
-	url(r'^class/$', 'class.views'),
-    url(r'^students/', 'students.views'),
-    url(r'^compositions/', 'compositions.views'),
-    url(r'^assignments/', 'assignments.views'),
+	url(r'^teacher/$', views.teacher),
+	url(r'^class/$', views.class),
+    url(r'^students/', views.students.),
+    url(r'^compositions/', views.compositions),
+    url(r'^assignments/', views.assignments),
     url(r'^admin/', include(admin.site.urls)),
 )
