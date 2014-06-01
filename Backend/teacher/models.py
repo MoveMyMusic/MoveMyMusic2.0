b4c5519f4769afd3270db5fdc19e16f808aa96e5
Andrew Fager <graveviolin@gmail.com>
2014-05-31 20:26:19 -0600

    Adding teacher app/api

--- Backend/teacher/models.py
+++ Backend/teacher/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Teacher(models.Model):
-	username = models.CharField(max_length=40, blank=True, default='')
-	teachername = models.CharField(max_length=40, blank=True, default='')
-	email = models.CharField(max_length=100, blank=True, default='')
-	password = models.CharField(max_length=40, blank=True, default='')
+	teacher_id = models.AutoField(primary_key=True, unique=True,)
+	username = models.CharField(max_length=40)
+	teacher_name = models.CharField(max_length=40)
+	email = models.EmailField(max_length=254)
+	password = models.CharField(max_length=40)
 
