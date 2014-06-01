d0e3e974674d7a89e3614fd4072342954c29bd58
Andrew Fager <graveviolin@gmail.com>
2014-06-01 00:04:13 -0600

    Correcting foreign key constraints

--- Backend/assignments/models.py
+++ Backend/assignments/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Assignment(models.Model):
 	assignment_id = models.AutoField(primary_key=True, unique=True)
-	class_id = models.ForeignKey('class.class_id')
+	class_id = models.ForeignKey('class',related_name='+')
 	#student_id = models.ForeignKey('students.student_id')
 	assigned = models.BooleanField()
-	answerkey_id = models.ForeignKey('compositions.composition_id')
-	studentresponse_id = models.ForeignKey('compositions.composition_id')
+	answerkey_id = models.ForeignKey('compositions',related_name='+')
+	studentresponse_id = models.ForeignKey('compositions',related_name='+')
