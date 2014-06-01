a4423cfe645cbae589ecb686c0a45a4356cff720
Andrew Fager <graveviolin@gmail.com>
2014-06-01 01:11:59 -0600

    Still fixing foreign keys

--- Backend/students/models.py
+++ Backend/students/models.py
@@ -4,4 +4,4 @@ class Student(models.Model):
 	student_id = models.AutoField(primary_key=True, unique=True,)
 	first_name = models.CharField(max_length=40)
 	last_name = models.CharField(max_length=40)
-	class_id = models.ForeignKey(Classroom)
+	class_id = models.ForeignKey('Classroom')
