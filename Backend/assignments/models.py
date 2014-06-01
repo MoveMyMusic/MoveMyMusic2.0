9373f3ef9905775f8ed02ff17be21d77ea66109f
Andrew Fager <graveviolin@gmail.com>
2014-06-01 01:16:32 -0600

    Still fixing foreign keys

--- Backend/assignments/models.py
+++ Backend/assignments/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Assignment(models.Model):
 	assignment_id = models.AutoField(primary_key=True, unique=True)
-	class_id = models.ForeignKey('Classroom')
+	class_id = models.ForeignKey('class.Classroom')
 	#student_id = models.ForeignKey('students.student_id')
 	assigned = models.BooleanField()
-	answerkey_id = models.ForeignKey('Compositions')
-	studentresponse_id = models.ForeignKey('Compositions')
+	answerkey_id = models.ForeignKey('compositions.Composition')
+	studentresponse_id = models.ForeignKey('compositions.Composition')
