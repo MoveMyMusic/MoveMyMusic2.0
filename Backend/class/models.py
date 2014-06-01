0dc2da209ac3e213fe4f4ec3824dc3ebfa17a373
Andrew Fager <graveviolin@gmail.com>
2014-06-01 00:58:59 -0600

    Still fixing foreign keys

--- Backend/class/models.py
+++ Backend/class/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Classroom():
+class Classroom(models.Model):
 	class_id = models.AutoField(primary_key=True, unique=True,)
 	teacher_id = models.ForeignKey(Teacher,related_name='+')
 	class_password = models.CharField(max_length=40)
